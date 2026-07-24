69ad865a735b70905ac96e9e606b05165b9891e8
K M VENKATACHALAM <darkbane.king@gmail.com>
2026-07-24 15:47:44 +0530

    Time: 11 ms (68.01%) | Memory: 44 MB (26.81%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length())
+        return false;
+        Map<Character,Character>map=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch2=t.charAt(i);
+            char ch1=s.charAt(i);
+            if(map.containsKey(ch1))
+            {
+                if(map.get(ch1)!=ch2)
+                return false;
+            }
+            else
+            {
+                if(map.containsValue(ch2))
+                return false;
+
+                else
+                map.put(ch1,ch2);
+            }
+        }
+        return true;
+    }
+}
