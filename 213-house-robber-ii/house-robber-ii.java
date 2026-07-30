4e5dfef18d7734d21ee5b7bea145db4f46634fb4
K M VENKATACHALAM <darkbane.king@gmail.com>
2026-07-30 09:47:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (90.51%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int rob(int[] nums) {
+     if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        
+        return Math.max(robRange(nums, 0, nums.length - 2), robRange(nums, 1, nums.length - 1));
+    }
+    
+    private int robRange(int[] nums, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+        
+        for (int i = start; i <= end; i++) {
+            int temp = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = temp;
+        }
+        
+        return prev1;
+    }
+}
+
+
+
+
+      
+
+        
+
+
+
+
+
+
+    
