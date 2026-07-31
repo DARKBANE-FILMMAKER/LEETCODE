5498a8380c324d64368cbd3d7bf4d6fdcb438724
K M VENKATACHALAM <darkbane.king@gmail.com>
2026-07-31 10:28:50 +0530

    Time: 1 ms (87.11%) | Memory: 45.5 MB (46.27%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,47 +1,30 @@
-import java.util.*;
-
 class Solution {
+    List<List<Integer>> res=new ArrayList<>();
 
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-
-        backtrack(nums, used, new ArrayList<>(), result);
-
-        return result;
+    public List<List<Integer>> permute(int[] nums)
+    {List<Integer> list=new ArrayList<>();
+        backtrack(nums,list);
+        return res;
     }
 
-    private void backtrack(int[] nums, boolean[] used,
-                           List<Integer> current,
-                           List<List<Integer>> result) {
-
-        if (current.size() == nums.length) {
-            result.add(new ArrayList<>(current));
+     public void backtrack(int[] nums,List<Integer> list)
+    {
+        if(list.size()==nums.length)
+        {
+            res.add(new ArrayList<>(list));
             return;
         }
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (used[i])
-                continue;
-
-            used[i] = true;
-            current.add(nums[i]);
-
-            backtrack(nums, used, current, result);
-
-            current.remove(current.size() - 1);
-            used[i] = false;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(list.contains(nums[i]))
+            continue;
+            list.add(nums[i]);
+            backtrack(nums,list);
+            list.remove(list.size()-1);
         }
     }
-}
- 
+}     
 
 
 
-
-
-
-
-
-
+  
